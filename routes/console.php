ea60ba19c38d37a3ef22b08f690e25f8bb5ccb88
John Eady <john.eady@gmail.com>
2026-02-05 15:18:37 -0500

    feat(health): add application health monitoring with spatie package

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
 
 Schedule::command('tickets:close-inactive')->everySixHours()->withoutOverlapping();
+
+Schedule::command(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyMinute();
